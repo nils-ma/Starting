58ec176aa8e65d95fb4f3c2e1261c541b96b4e84
nils-ma <nils.matysek2@web.de>
2025-07-02 13:14:20 +0200

    Fehlender code

--- dice6.java
+++ dice6.java
@@ -4,8 +4,22 @@ public class dice6 {
 
         double randomNumber = Math.random();  // inklusive 0 [0.0 , 1.0) exklusive 1
         double correctLength = randomNumber * 6;  // [0.0 , 6.0)
-        double correctInterval = correctLength + 1;  // [1.0 , 7.0)
-        int correctResult = (int) correctInterval;
+        double correctInterval = correctLength + 1;  // [1.0 , 7.0) exklusive 7
+        int correctResult = (int) correctInterval;  // Ausgabe als int
+
+        // Einfachere Option in 1 Zeile
+        int result = (int) (6 * Math.random() + 1);
+
+
+        // Zählen wie oft bis zur 6
+        int resultNeu;
+        int numberOfRolls = 0;
+
+        do{
+            int resultNeu = (int) (6 * Math.random() + 1);
+            numberOfRolls = numberOfRolls + 1;
+        } while(resultNeu != 6);
+
 
         System.out.println("Würfelergebnis ist " + correctResult);
 
